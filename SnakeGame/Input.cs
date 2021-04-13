2c4bbad37c4b386ed364c8aed50143504032991e
Liam <81374620+LiamMCode@users.noreply.github.com>
2021-04-13 21:06:56 +0100

    code written class files not in project, why?
    This code was written a while ago the class files aren't in the same project as the form

---
+++ SnakeGame/Input.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections; // The hashtable class is in the collections
+using System.Windows.Forms; // The keys class is in the Forms 
+
+namespace SnakeGame
+{
+    class Input
+    {
+        // creating a new instance of Hashtable class this class is used to optimize the keys inserted in it
+        private static Hashtable keyTable = new Hashtable(); 
+
+        public static bool KeyPress(Keys key)
+        {
+            // this function will return a key back to the class
+
+            if (keyTable[key] == null)
+            {
+                // if the hashtable is empty then we return flase
+                return false;
+            }
+            // if the hastable is not empty then we return true
+            return (bool)keyTable[key];
+        }
+
+        // this function will change state of the keys and the player with it this function has two arguments Key and state
+        public static void changeState(Keys key, bool state)
+        {
+            keyTable[key] = state;
+        }
+
+    }
+}
